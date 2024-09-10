4140e9b6cb4fa07e398d021f3a50de04140708db
Carlos Marrugo <140767174+Carlos-Marrugo@users.noreply.github.com>
2024-09-10 12:37:10 -0500

    Update Main.java

--- ejercicio#2-contactos-ArrayList/Main.java
+++ ejercicio#2-contactos-ArrayList/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Contacto> listaDeContactos = new ArrayList<>();
 
+        listaDeContactos.add(new Contacto("Juan", "123456789"));
+        listaDeContactos.add(new Contacto("Ana", "987654321"));
+        listaDeContactos.add(new Contacto("Pedro", "555666777"));
+        listaDeContactos.add(new Contacto("Laura", "888999000"));
+
+    }
+}
