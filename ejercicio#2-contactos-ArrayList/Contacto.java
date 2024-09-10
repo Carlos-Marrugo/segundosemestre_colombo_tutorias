427c77b5d3548559c59185073bcf7936a3678c34
Carlos Marrugo <140767174+Carlos-Marrugo@users.noreply.github.com>
2024-09-10 16:14:04 -0500

    Update Contacto.java

--- ejercicio#2-contactos-ArrayList/Contacto.java
+++ ejercicio#2-contactos-ArrayList/Contacto.java
@@ -1,1 +1,26 @@
+public class Contacto {
+    private String nombre;
+    private String telefono;
+
+    // Constructor
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    // Metodo toString para facilitar la imppresion de los datos
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Teléfono: " + telefono;
+    }
+}
 
