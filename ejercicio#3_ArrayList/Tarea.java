881ecf4d3ce3926bb42c5b50f4d99f858c59ae4a
Carlos Marrugo <140767174+Carlos-Marrugo@users.noreply.github.com>
2024-09-11 01:15:30 -0500

    Update Tarea.java

--- ejercicio#3_ArrayList/Tarea.java
+++ ejercicio#3_ArrayList/Tarea.java
@@ -1,1 +1,33 @@
 
+public class Tarea {
+
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void marcarComoCompletada() {
+        this.completada = true;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion + (completada ? " (Completada)" : " (Pendiente)");
+    }
+}
+
